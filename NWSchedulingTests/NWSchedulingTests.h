54388c7be6445b8c0a55ca5a4d2f15683bfeeaf8
TechieTotoro <techietotoro@gmail.com>
2013-03-23 12:19:27 -1000

    Added new NWScheduling library. Added NWScheduler class. Added unit tests for NWScheduling.

---
+++ NWSchedulingTests/NWSchedulingTests.h
@@ -0,0 +1,13 @@
+//
+//  NWSchedulingTests.h
+//  NWSchedulingTests
+//
+//  Created by Nathanael Wallace on 3/23/13.
+//
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface NWSchedulingTests : SenTestCase
+
+@end
---
+++ NWSchedulingTests/NWSchedulingTests.m
@@ -0,0 +1,32 @@
+//
+//  NWSchedulingTests.m
+//  NWSchedulingTests
+//
+//  Created by Nathanael Wallace on 3/23/13.
+//
+//
+
+#import "NWSchedulingTests.h"
+
+@implementation NWSchedulingTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in NWSchedulingTests");
+}
+
+@end
