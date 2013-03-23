cdf96a71ab7b8e478bb297db2a528440c3f0f090
TechieTotoro <techietotoro@gmail.com>
2013-03-23 12:05:23 -1000

    Added new build target for testing, and a README file.

---
+++ NWSchedulingTest/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  NWSchedulingTest
+//
+//  Created by Nathanael Wallace on 3/23/13.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
---
+++ README.txt
@@ -0,0 +1,6 @@
+NWScheduling
+
+Created by Nathan W (@techietotoro) on 3/23/13
+-------
+
+Simply copy the classes into your project and import them to use.
